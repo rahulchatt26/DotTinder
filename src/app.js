77f18ab791c4122390e6f83b666d03075f2e663e
Rahul Chatterjee <chatterjeerahul2604@gmail.com>
2025-06-29 19:18:29 +0530

    created user schema and model

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const connectDB = require("./config/database");
-
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  try {
+    const ourFirstUser = new User({
+      firstName: "Tiasha",
+      lastName: "Banerjee",
+      emailId: "tiasha@gmail.com",
+      password: "Tiasha@123",
+    });
+    await ourFirstUser.save();
+
+    res.status(201).send("User created successfully");
+  } catch (err) {
+    return res.status(400).json({ ERROR: err.message });
+  }
+});
 
 connectDB()
   .then(() => {
